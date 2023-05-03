459a2e09c0978673c39ade6860c3d470d2918091
parinrava <pravanbakhsh@my.bcit.ca>
2023-05-03 10:30:07 -0700

    small change

--- databaseconnection.js
+++ databaseconnection.js
@@ -5,6 +5,6 @@ const mongodb_user = process.env.MONGODB_USER;
 const mongodb_password = process.env.MONGODB_PASSWORD;
 
 const MongoClient = require("mongodb").MongoClient;
-const atlasURI = `mongodb+srv://parinrava:12w34r56@cluster0.a0i4o8k.mongodb.net/2537Assign1?retryWrites=true`;
+const atlasURI = `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/?retryWrites=true`;
 var database = new MongoClient(atlasURI, {useNewUrlParser: true, useUnifiedTopology: true});
 module.exports = {database};
