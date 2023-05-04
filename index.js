abcef8b5a19374d1e18fa581f8c2ecbb23dd797f
parinrava <pravanbakhsh@my.bcit.ca>
2023-05-03 21:56:48 -0700

    try

--- index.js
+++ index.js
@@ -118,7 +118,10 @@ app.post('/submitUser', async(req,res) => {
     var email = req.body.email;
        
     if (!name) {
-                res.redirect('/signup?missing=name');
+        const htmlText = '<h1>name is missing</h1><';
+
+        // res.setHeader('Content-Type', 'text/html');
+            res.send(htmlText);
                 
             } else if (!email) {
                 res.redirect('/signup?missing=email');
